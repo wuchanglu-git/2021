dbbbff39e9a2d0b5d64ef5c7aba2cf9b0f15ce56
吴昌禄 <3099890837@qq.com>
2021-02-12 19:04:45 +0800

    bui小玩具调整

--- demo/bui/index.js
+++ demo/bui/index.js
@@ -2,6 +2,7 @@ const DRAWING = true
 const NOT_DRAWING = false
 let isDrawing = NOT_DRAWING
 let lastPosition = {}
+let tempPositions = []
 let positionList = []
 
 function addPosition(start, end, list) {
@@ -20,7 +21,7 @@ function mouseMove(e) {
     if (isDrawing === DRAWING) {
         const endPosition = getOffsetPosition(e)
         draw(ctx, lastPosition, endPosition)
-        addPosition(lastPosition, endPosition, positionList)
+        addPosition(lastPosition, endPosition, tempPositions)
         lastPosition = endPosition
     }
 }
@@ -43,25 +44,68 @@ function draw(ctx, startPosition, endPosition) {
 }
 
 function onSaveButtonClick() {
-    console.log(JSON.stringify(positionList))
+    positionList.push([tempPositions])
+    tempPositions = []
 }
 
 function onResetButtonClick() {
     positionList = []
+    tempPositions = []
     bui.height = 500
     ctx.lineWidth = 3
 }
 
+function onBackButtonClick() {
+    tempPositions = []
+    bui.height = 500
+    ctx.lineWidth = 3
+        //读取positionList重新渲染
+    const arr = positionList.flat(Infinity)
+    arr.forEach(val => {
+        draw(ctx, val.start, val.end)
+    })
+}
+
+function onRenderButtonClick() {
+    check()
+    const arr = positionList.flat(Infinity)
+    let index = 0
+
+    function renderFn() {
+        if (arr[index]) {
+            requestAnimationFrame(() => {
+                draw(renderCtx, arr[index].start, arr[index].end)
+                index++
+                renderFn()
+            })
+        }
+    }
+    renderFn()
+}
+
+function check() {
+    if (tempPositions.length) {
+        positionList.push([tempPositions])
+        tempPositions = []
+    }
+}
+
 const bui = document.querySelector('#bui')
+const renderCanvas = document.querySelector('#renderCanvas')
 const save = document.querySelector('#save')
 const reset = document.querySelector('#reset')
-
+const back = document.querySelector('#back')
+const render = document.querySelector('#render')
 
 var ctx = bui.getContext("2d")
 ctx.lineWidth = 3
+var renderCtx = renderCanvas.getContext('2d')
+renderCtx.lineWidth = 3
 bui.addEventListener('mousedown', mouseDown)
 bui.addEventListener('mousemove', mouseMove)
 bui.addEventListener('mouseup', mouseUp)
 
 save.addEventListener('click', onSaveButtonClick)
 reset.addEventListener('click', onResetButtonClick)
+back.addEventListener('click', onBackButtonClick)
+render.addEventListener('click', onRenderButtonClick)
