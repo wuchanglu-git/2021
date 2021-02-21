10330d12801fbc53b80ace2dc5361a4b46a4663c
吴昌禄 <3099890837@qq.com>
2021-02-21 13:27:42 +0800

    引入html-webpack-plugin 去自动生成html文件

--- webpack/spa-react/webpack.config.js
+++ webpack/spa-react/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 //引入插件cleanwebpackplugin清除之前的dist文件
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     main: path.join(__dirname, "src/index.js"),
@@ -11,7 +12,13 @@ module.exports = {
     //文件名称
     filename: "bundle.js",
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html", //最终创建的文件名
+      template: path.join(__dirname, "src/index.html"), //指定模板路径
+    }),
+  ],
   module: {
     rules: [
       {
