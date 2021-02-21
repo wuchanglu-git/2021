b715c2ef10a9e26470060ef4225bde719b264968
吴昌禄 <3099890837@qq.com>
2021-02-21 13:14:11 +0800

    react引入presets文件修改

--- webpack/spa-react/src/index.js
+++ webpack/spa-react/src/index.js
@@ -1,3 +1,4 @@
-import {Home} from "./views/Home.jsx";
-console.log("start");
-console.log(Home);
+import React from "react";
+import ReactDom from "react-dom";
+import { Home } from "./views/Home.jsx";
+ReactDom.render(<Home />, document.querySelector("#root"));
